508996c477d142c91b422f52f314c466a2a62dbb
Typhon <loic.sculier@hotmail.fr>
2017-11-17 11:51:00 +0100

    Pulse

---
+++ src/animatefx/animation/Pulse.java
@@ -0,0 +1,47 @@
+package animatefx.animation;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+
+/**
+ * @author Loïc Sculier
+ */
+public class Pulse {
+
+    /**
+     * Create new Pulse
+     *
+     * @param node The node to affect
+     */
+    public Pulse(Node node) {
+        Pulse(node);
+    }
+
+    private void Pulse(Node node) {
+
+        Timeline t =
+                new Timeline(
+                        new KeyFrame(Duration.millis(0),
+                                new KeyValue(node.scaleXProperty(), 1),
+                                new KeyValue(node.scaleYProperty(), 1),
+                                new KeyValue(node.scaleZProperty(), 1)
+                        ),
+                        new KeyFrame(Duration.millis(500),
+                                new KeyValue(node.scaleXProperty(), 1.05),
+                                new KeyValue(node.scaleYProperty(), 1.05),
+                                new KeyValue(node.scaleZProperty(), 1.05)
+                        ),
+                        new KeyFrame(Duration.millis(1000),
+                                new KeyValue(node.scaleXProperty(), 1),
+                                new KeyValue(node.scaleYProperty(), 1),
+                                new KeyValue(node.scaleZProperty(), 1)
+                        )
+                );
+        t.play();
+    }
+}
+
