427df62bc323be1b5fcea7ba68cfc1b00f1c342a
Typhon <loic.sculier@hotmail.fr>
2017-11-20 14:05:31 +0100

    BounceOutRight

---
+++ src/animatefx/animation/BounceOutRight.java
@@ -0,0 +1,45 @@
+package animatefx.animation;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+/**
+ * @author Loïc Sculier
+ */
+public class BounceOutRight {
+
+    /**
+     * Create new BounceOutRight animation
+     *
+     * @param node The node to affect
+     */
+    public BounceOutRight(final Node node){
+        BounceOutRight(node);
+    }
+
+
+
+    private static void BounceOutRight(Node node) {
+        double endX = node.getScene().getWidth() - node.localToScene(0, 0).getX();
+
+        Timeline t = new Timeline(
+                new KeyFrame(Duration.millis(0),
+                        new KeyValue(node.opacityProperty(), 1)
+                ),
+                new KeyFrame(Duration.millis(200),
+                        new KeyValue(node.opacityProperty(), 1),
+                        new KeyValue(node.translateXProperty(), -20)
+                ),
+                new KeyFrame(Duration.millis(450),
+                        new KeyValue(node.opacityProperty(), 0),
+                        new KeyValue(node.translateXProperty(), endX)
+                )
+
+        );
+        t.play();
+    }
+
+}
