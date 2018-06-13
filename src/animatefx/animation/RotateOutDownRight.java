063225a7e081a434d27082c54e3273f0810648f2
Typhon <loic.sculier@hotmail.fr>
2018-06-13 14:29:40 +0200

    RotateOut categorie

---
+++ src/animatefx/animation/RotateOutDownRight.java
@@ -0,0 +1,41 @@
+package animatefx.animation;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.Node;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
+
+/**
+ * @author Loïc Sculier aka typhon0
+ */
+public class RotateOutDownRight {
+
+    /**
+     * Create new RotateOutDownRight
+     *
+     * @param node The node to affect
+     */
+    public RotateOutDownRight(Node node) {
+        RotateOutDownRight(node);
+    }
+
+    private void RotateOutDownRight(Node node) {
+        node.setRotationAxis(Rotate.Z_AXIS);
+        Rotate rotate = new Rotate(0, node.getBoundsInLocal().getWidth(), node.getBoundsInLocal().getHeight());
+        node.getTransforms().add(rotate);
+        Timeline t =
+                new Timeline(
+                        new KeyFrame(Duration.millis(0),
+                                new KeyValue(rotate.angleProperty(), 0, AnimateFXInterpolator.EASE),
+                                new KeyValue(node.opacityProperty(), 1, AnimateFXInterpolator.EASE)
+                        ),
+                        new KeyFrame(Duration.millis(1000),
+                                new KeyValue(rotate.angleProperty(), -45, AnimateFXInterpolator.EASE),
+                                new KeyValue(node.opacityProperty(), 0, AnimateFXInterpolator.EASE)
+                        )
+                );
+        t.play();
+    }
+}
