edf5698e147dbaa7e6f34f6c0e9b4a497d31ab7f
negste <ste.negri.liste@gmail.com>
2018-10-02 11:03:41 +0200

    new animation that makes a labeled "glow" (alternate between two colors)

---
+++ animatefx/src/main/java/animatefx/animation/GlowText.java
@@ -0,0 +1,52 @@
+package animatefx.animation;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.control.Labeled;
+import javafx.scene.paint.Paint;
+import javafx.util.Duration;
+
+/**
+ * Makes text glow alternating between two colors
+ *
+ * @author ced04u
+ */
+public class GlowText extends AnimationFX {
+
+    private final Paint originalPaint;
+    private final Paint colorA;
+    private final Paint colorB;
+
+    public GlowText(Labeled node, Paint colorA, Paint colorB) {
+        super(node);
+        this.originalPaint = getNode().textFillProperty().get();
+        this.colorA = colorA;
+        this.colorB = colorB;
+        getTimeline().getKeyFrames().addAll(
+                new KeyFrame(Duration.millis(0),
+                        new KeyValue(getNode().textFillProperty(), colorA)),
+                new KeyFrame(Duration.millis(500),
+                        new KeyValue(getNode().textFillProperty(), colorB)),
+                new KeyFrame(Duration.millis(1000),
+                        new KeyValue(getNode().textFillProperty(), colorA))
+        );
+    }
+
+    @Override
+    public Labeled getNode() {
+        return (Labeled) super.getNode();
+    }
+
+    @Override
+    AnimationFX resetNode() {
+        getNode().setTextFill(originalPaint);
+        return this;
+    }
+
+    @Override
+    void initTimeline() {
+        setTimeline(new Timeline()); //will be populated at the end of constructor
+    }
+
+}
