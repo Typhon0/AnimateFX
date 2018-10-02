e81dda5d89ae72a3d85b9ad66d07a9aeefaaf544
negste <ste.negri.liste@gmail.com>
2018-10-02 13:52:20 +0200

    removed unused import

--- animatefx-demo/src/main/java/animatefx/demo/Controller.java
+++ animatefx-demo/src/main/java/animatefx/demo/Controller.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 
 
 public class Controller {
