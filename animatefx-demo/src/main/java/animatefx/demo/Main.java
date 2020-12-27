e73548f025eb8e25abc31d5a018e3fdc3420495f
Harsh3305 <harshverma3305@gmail.com>
2020-12-27 15:21:37 +0530

    Added java icon in animatefx-demo

--- animatefx-demo/src/main/java/animatefx/demo/Main.java
+++ animatefx-demo/src/main/java/animatefx/demo/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
         primaryStage.setTitle("AnimateFX Demo");
         primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image("java.png"));
     }
 
 
