75828b53a78991043ee62ddbfdb3467cb3cd0345
41zu <dominikator.mueka@googlemail.com>
2024-06-01 12:33:22 +0200

    add module info

---
+++ animatefx-demo/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module animatefx.demo {
+    requires animatefx;
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+
+    opens animatefx.demo;
+}
---
+++ animatefx/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module animatefx {
+    requires transitive javafx.controls;
+
+    exports animatefx.animation;
+    exports animatefx.animation.partial;
+    exports animatefx.modifier;
+    exports animatefx.util;
+}
